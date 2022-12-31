1b9ae97e9c25f63e82ecae0ed763f9f02e69eb4a
ayamoosa <ayamoosa98@gmail.com>
2022-12-31 14:20:05 -0800

    fix 500 err

---
+++ server/server.js
@@ -0,0 +1,19 @@
+/* eslint-disable semi */
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// placeholder
+app.get('/api', (req, res) => {
+
+  res.json({ message: 'Hello from server!' });
+});
+
+app.listen(PORT, () => {
+  console.log('Server listening on', PORT);
+});
+
+
+
